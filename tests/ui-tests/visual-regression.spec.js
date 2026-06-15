eacf405aabaf67499769555636a2058ed5ccddce
Cody U <cody.urq@gmail.com>
2026-06-15 14:57:57 -0700

    percy fix

--- tests/ui-tests/visual-regression.spec.js
+++ tests/ui-tests/visual-regression.spec.js
@@ -2,71 +2,36 @@ const { test, expect } = require("../../fixtures/pages.fixture");
 const percySnapshot = require("@percy/playwright");
 const HomePage = require("../../POM/pages/HomePage");
 
-test.describe("Visual regression @percy", () => {
+test.describe('Visual regression "logged out"', () => {
   test("Home page logged out @percy", async ({ page, homePage }, testInfo) => {
     await homePage.openHome();
-    await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
+    await page.waitForLoadState('domcontentloaded');
+    await percySnapshot(page, `Home Page logged out - ${testInfo.project.name}`);
   });
+});
+test.describe('Visual Regression Tests "logged in"', () => {
+  test.use({ storageState: 'playwright/.auth/user.json'});
   test("Home page logged in @percy", async ({
     page,
     homePage,
-    _apiLogin,
   }, testInfo) => {
     await homePage.openHome();
-    await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
+    await percySnapshot(page, `Home Page logged in - ${testInfo.project.name}`);
   });
   test("Signup page logged in @percy", async ({
     page,
     homePage,
-    _apiLogin,
   }, testInfo) => {
     await homePage.openHome();
     await page.goto("/signup");
-    await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
+    await percySnapshot(page, `Signup Page - ${testInfo.project.name}`);
   });
   test("Pricing page logged in @percy", async ({
     page,
     homePage,
-    _apiLogin,
   }, testInfo) => {
     await homePage.openHome();
     await page.goto("/signup?v=pricing");
-    await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
-  });
-
-  test.describe("@mobile Visual regression @percy", () => {
-    test("Home page logged in @percy @mobile", async ({
-      page,
-      homePage,
-      _apiLogin,
-    }, testInfo) => {
-      await homePage.openHome();
-      await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
-    });
-    test("@mobile Home page logged out @percy", async ({
-      page,
-      homePage,
-    }, testInfo) => {
-      await homePage.openHome();
-      await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
-    });
-    test("@mobile Signup page logged in @percy", async ({
-      page,
-      homePage,
-      _apiLogin,
-    }, testInfo) => {
-      await homePage.openHome();
-      await page.goto("/signup");
-      await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
-    });
-    test("@mobile Pricing page logged in @percy", async ({
-      page,
-      homePage,
-      _apiLogin,
-    }, testInfo) => {
-      await homePage.openHome();
-      await page.goto("/signup?v=pricing");
-      await percySnapshot(page, `Home Page - ${testInfo.project.name}`);
-    });
+    await percySnapshot(page, `Pricing Page - ${testInfo.project.name}`);
   });
 });
