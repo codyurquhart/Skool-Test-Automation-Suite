f2f4800cf85d93d1ff3dfc9513c27fc604977143
Cody U <cody.urq@gmail.com>
2026-06-16 13:19:48 -0700

    fix

--- playwright.percy.config.js
+++ playwright.percy.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     baseURL: process.env.BASE_URL,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    video: off,
     actionTimeout: 15_000,
     navigationTimeout: 30_000,
   },
